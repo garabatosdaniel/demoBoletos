5d0fddd1280fe76edb68b1ccc4d5d2dbfffdbb6f
garabatosdaniel <danielramirezhinojosa@gmail.com>
2014-10-02 13:26:31 -0500

    Lista de salidas a color

--- app/scripts/controllers/compraboletos.js
+++ app/scripts/controllers/compraboletos.js
@@ -12,8 +12,10 @@ angular.module('boletosApp')
     $scope.showResult = false;
     $scope.estates = [{'id':2,'name':'Aguascalientes'},{'id':3,'name':'Baja California'},{'id':4,'name':'Baja California Sur'},{'id':5,'name':'Campeche'},{'id':50,'name':'Categoria Nacional'},{'id':8,'name':'Chiapas'},{'id':9,'name':'Chihuahua'},{'id':6,'name':'Coahuila de Zaragoza'},{'id':7,'name':'Colima'},{'id':1,'name':'Distrito Federal'},{'id':10,'name':'Durango'},{'id':11,'name':'Guanajuato'},{'id':12,'name':'Guerrero'},{'id':13,'name':'Hidalgo'},{'id':14,'name':'Jalisco'},{'id':16,'name':'Michoacán de Ocampo'},{'id':17,'name':'Morelos'},{'id':15,'name':'México'},{'id':18,'name':'Nayarit'},{'id':19,'name':'Nuevo León'},{'id':20,'name':'Oaxaca'},{'id':21,'name':'Puebla'},{'id':22,'name':'Querétaro'},{'id':23,'name':'Quintana Roo'},{'id':24,'name':'San Luis Potosí'},{'id':25,'name':'Sinaloa'},{'id':26,'name':'Sonora'},{'id':27,'name':'Tabasco'},{'id':28,'name':'Tamaulipas'},{'id':29,'name':'Tlaxcala'},{'id':30,'name':'Veracruz de Ignacio de la Llave'},{'id':31,'name':'Yucatán'},{'id':32,'name':'Zacatecas'}];
     $scope.serchResult = [
-      {horario:'9:30am', terminalSalida:'DF', terminalLlegada:'Guerrero', precio: '500.50', tiempoEstidmado:'10.5hrs', disponibles:'5/40'},
-      {horario:'9:30am', terminalSalida:'Guerrero', terminalLlegada:'Veracruz', precio: '500.50', tiempoEstidmado:'10.5hrs', disponibles:'5/40'}
+      {horario:'9:30', terminalSalida:'DF', terminalLlegada:'Guerrero', precio: '500.50', tiempoEstidmado:'10.5hrs', disponibles:'5/40'},
+      {horario:'10:00', terminalSalida:'Guerrero', terminalLlegada:'Veracruz', precio: '500.50', tiempoEstidmado:'10.5hrs', disponibles:'5/40'},
+      {horario:'19:30', terminalSalida:'Guerrero', terminalLlegada:'Veracruz', precio: '500.50', tiempoEstidmado:'10.5hrs', disponibles:'5/40'},
+      {horario:'21:30', terminalSalida:'Guerrero', terminalLlegada:'Veracruz', precio: '500.50', tiempoEstidmado:'10.5hrs', disponibles:'5/40'}
     ];
     $scope.buscaBoletos = function(){
       $scope.showResult = true;
--- app/scripts/controllers/listasalidas.js
+++ app/scripts/controllers/listasalidas.js
@@ -10,8 +10,12 @@
 angular.module('boletosApp')
   .controller('ListasalidasCtrl', function ($scope) {
     $scope.salidas = [
-    {departure:'21:10', arrival:'07:30',destino:'Tampico', escalas:'Villahermosa - Veracruz', disponibles:5, total:40, puerta: 'B100'},
+    {departure:'21:10', arrival:'07:30',destino:'Tampico', escalas:'Villahermosa - Veracruz', disponibles:5, total:40, puerta: 'B2'},
     {departure:'21:20', arrival:'04:30',destino:'Veracruz', escalas:'Villahermosa',disponibles:3, total:40, puerta: 'A2'},
-    {departure:'21:30', arrival:'05:30', destino:'Tampico', escalas: '', disponibles:5, total:40, puerta: '10'}
+    {departure:'21:30', arrival:'05:30', destino:'Tampico', escalas: '', disponibles:5, total:40, puerta: '10'},
+    {departure:'21:30', arrival:'05:30', destino:'Tampico', escalas: '', disponibles:1, total:20, puerta: 'A1'},
+    {departure:'21:30', arrival:'05:30', destino:'Tampico', escalas: '', disponibles:5, total:10, puerta: 'A5'},
+    {departure:'21:30', arrival:'05:30', destino:'Tampico', escalas: '', disponibles:4, total:50, puerta: 'B0'}
+
     ];
   });
